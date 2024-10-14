310cea47a93ef21a56fd28c836e7372a9e1c47b8
ChaosMigu <93799340+ChaosMigu@users.noreply.github.com>
2024-10-14 07:21:51 +0200

    control

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    public float moveSpeed = 10f;
+    public float maxVelocity = 5f;
+    private Rigidbody playerRigidbody;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Obtener la dirección de la cámara
+        Vector3 forward = camera.transform.forward;
+        forward.y = 0; // Ignorar el eje Y
+        forward.Normalize();
+
+        Vector3 right = camera.transform.right;
+        right.y = 0;
+        right.Normalize();
+
+        // Capturar las entradas
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Combinar movimiento con la dirección de la cámara
+        Vector3 movement = (forward * moveVertical) + (right * moveHorizontal);
+
+        // Aplicar fuerza al Rigidbody del jugador
+        if (playerRigidbody.velocity.magnitude < maxVelocity) // Limitar la velocidad máxima
+        {
+            playerRigidbody.AddForce(movement * moveSpeed);
+        }
+    }
+}
